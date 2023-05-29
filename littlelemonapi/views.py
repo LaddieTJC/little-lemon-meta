5665af3b7bf21bf087fb5a3a27246a293d312aea
Laddie <laddietanjicheng@gmail.com>
2023-05-29 16:41:14 +0800

    added add new managers api

--- littlelemonapi/views.py
+++ littlelemonapi/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import MenuItem, Category
-from rest_framework import generics
+from rest_framework import generics, status
 from .serializers import MenuItemSerializer, CategorySerializer, ManagerSerializer
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from django.contrib.auth.models import User, Group
 from .permissions import ManagerOnlyPermission
+from rest_framework.response import Response
 # Create your views here.
 
 
@@ -30,10 +31,28 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
         return [IsAuthenticated()]
     
 
-class ManagersView(generics.ListAPIView):
-    queryset = User.objects.filter(groups__name='Manager')
-    serializer_class = ManagerSerializer
+class ManagersView(generics.GenericAPIView):
     throttle_classes = [AnonRateThrottle, UserRateThrottle]
     permission_classes = [ManagerOnlyPermission,]
+    
+    def get(self, request):
+        users = User.objects.filter(groups__name='Manager')
+        serializer = ManagerSerializer(users, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        username = request.data['username']
+        if username:
+            user = get_object_or_404(User, username=username)
+            managers = Group.objects.get(name="Manager")
+            managers.user_set.add(user)
+            return Response({"Message":"Added to the Manager Group"})
+        
+        return Response({"Message":"Error"}, status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
 
     
